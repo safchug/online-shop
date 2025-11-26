73f55210c05b828ccc0d873bc20b2e926b886a59
Vasyl Savchuk <safchug@gmail.com>
2025-11-26 11:00:27 +0200

    removed unused imports

--- apps/services/auth-service/src/auth/auth.service.ts
+++ apps/services/auth-service/src/auth/auth.service.ts
@@ -1,9 +1,4 @@
-import {
-  Injectable,
-  UnauthorizedException,
-  ConflictException,
-  NotFoundException,
-} from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { RpcException } from "@nestjs/microservices";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
